6b944c7cf288ecdb735798d721672be747e54aae
Tasmiya <tasmiyairfan4280@.com>
2025-04-24 13:42:16 +0500

    first commit

---
+++ providers.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+data "aws_eks_cluster_auth" "auth" {
+  name = module.eks.cluster_name
+  depends_on = [module.eks]   # still good practice
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  token                  = data.aws_eks_cluster_auth.auth.token
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+}
+
---
+++ vpc.tf
@@ -0,0 +1,34 @@
+
+data "aws_availability_zones" "azs" {}
+
+
+module "eks-vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.21.0"
+
+  name = "eks-vpc"
+  cidr = var.vpc_cidr_block
+
+  private_subnets = var.private_subnet_cidr_blocks
+  public_subnets = var.public_subnet_cidr_blocks
+
+  azs = data.aws_availability_zones.azs.names
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  enable_dns_hostnames = true
+
+
+  tags = {
+    "kubernetes.io/cluster/mosque-ai-eks-cluster" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/mosque-ai-eks-cluster" = "shared"
+    "kubernetes.io/cluster/elb" = 1
+  }
+  private_subnet_tags = { 
+   "kubernetes.io/cluster/mosque-ai-eks-cluster" = "shared"
+    "kubernetes.io/cluster/eks" = 1
+  }
+}
